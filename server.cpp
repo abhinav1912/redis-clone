90f6f236c62bb856ddc0ad5be6003375936fe0ab
abhinav1912 <abhinavmathur1912@gmail.com>
2023-02-27 22:21:20 +0530

    refactor: added Server class

--- server.cpp
+++ server.cpp
@@ -4,6 +4,7 @@
 #include<netinet/in.h>
 #include<unistd.h>
 #include<errno.h>
+#include "Node.h"
 
 #define PORT 8080
 #define MAXCONN 3
@@ -15,53 +16,48 @@ static void log_error_and_abort(const char *msg) {
     abort();
 }
 
-static void handle_connection(int connfd) {
-    char readbuf[MESSAGE_LEN] = {};
-    int n = read(connfd, readbuf, MESSAGE_LEN);
-    if (n < 0) {
-        perror("Error while reading");
-        return;
+class Server: Node {
+    void handle_connection(int connfd) {
+        char readbuf[MESSAGE_LEN] = {};
+        int n = read(connfd, readbuf, MESSAGE_LEN);
+        if (n < 0) {
+            perror("Error while reading");
+            return;
+        }
+        printf("Client %d's message: %s\n", connfd, readbuf);
+        char response_buf[] = "Response!";
+        write(connfd, response_buf, sizeof(response_buf));
+    };
+
+    public:
+    void listen_for_connections() {
+        int sockfd = get_socket();
+        struct sockaddr_in addr = get_address(PORT, INADDR_ANY);
+
+        if (bind(sockfd, (const sockaddr *) &addr, sizeof(addr)) < 0) {
+            log_error_and_abort("Error while binding");
+        }
+
+        int recv = listen(sockfd, MAXCONN);
+        if (recv < 0) {
+            log_error_and_abort("Error opening listener");
+        }
+        while (true) {
+            struct sockaddr client_addr = {};
+            socklen_t socklen = sizeof(client_addr);
+            int connfd = accept(sockfd, (sockaddr *) &client_addr, (socklen_t *) &socklen);
+            if (connfd < 0) {
+                log_error_and_abort("Error while accepting connection");
+                continue;
+            }
+            handle_connection(connfd);
+            close(connfd);
+        }
     }
-    printf("Client %d's message: %s\n", connfd, readbuf);
-    char response_buf[] = "Response!";
-    write(connfd, response_buf, sizeof(response_buf));
-}
+};
 
 int main() {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0), opt = 1;
-    if (sockfd < 0) {
-     log_error_and_abort("Error while opening socket");
-    }
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-     log_error_and_abort("Error setting socket options");
-    }
-
-    struct sockaddr_in addr = {};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(sockfd, (const sockaddr *) &addr, sizeof(addr)) < 0) {
-     log_error_and_abort("Error while binding");
-        
-    }
-
-    int recv = listen(sockfd, MAXCONN);
-    if (recv < 0) {
-     log_error_and_abort("Error opening listener");
-    }
-
-    while (true) {
-        struct sockaddr client_addr = {};
-        socklen_t socklen = sizeof(client_addr);
-        int connfd = accept(sockfd, (sockaddr *) &client_addr, (socklen_t *) &socklen);
-        if (connfd < 0) {
-         log_error_and_abort("Error while accepting connection");
-            continue;
-        }
-        handle_connection(connfd);
-        close(connfd);
-    }
+    Server server = Server();
+    server.listen_for_connections();
     return 0;
 }
