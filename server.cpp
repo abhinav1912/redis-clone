db21f90c9c364eaa52dfd3d7d320214e459de73d
abhinav1912 <abhinavmathur1912@gmail.com>
2023-02-28 20:12:25 +0530

    wip: added single request handling for server

--- server.cpp
+++ server.cpp
@@ -4,11 +4,12 @@
 #include<netinet/in.h>
 #include<unistd.h>
 #include<errno.h>
+#include<cstring>
 #include "Node.h"
 
 #define PORT 8080
 #define MAXCONN 3
-#define MESSAGE_LEN 64
+#define MAX_MESSAGE_LEN 4096
 
 static void log_error_and_abort(const char *msg) {
     int err = errno;
@@ -17,16 +18,36 @@ static void log_error_and_abort(const char *msg) {
 }
 
 class Server: Node {
-    void handle_connection(int connfd) {
-        char readbuf[MESSAGE_LEN] = {};
-        int n = read(connfd, readbuf, MESSAGE_LEN);
-        if (n < 0) {
-            perror("Error while reading");
-            return;
+    int single_request(int connfd) {
+        char rbuf[4 + MAX_MESSAGE_LEN + 1] = {};
+        int rv = read_full(connfd, rbuf, 4);
+        if (rv) {
+            log_message("read() error");
+            return rv;
         }
-        printf("Client %d's message: %s\n", connfd, readbuf);
-        char response_buf[] = "Response!";
-        write(connfd, response_buf, sizeof(response_buf));
+        uint32_t len = 0;
+        memcpy(&len, rbuf, 4);
+        if (len > MAX_MESSAGE_LEN) {
+            log_message("Message too long");
+            return -1;
+        }
+
+        rv = read_full(connfd, &rbuf[4], len);
+        if (rv) {
+            log_message("read() error");
+            return rv;
+        }
+
+        // handle request
+        rbuf[4 + len] = '\0';
+        printf("Client %d says: %s\n", connfd, rbuf[4]);
+
+        const char reply[] = "Served response!";
+        char writebuf[4 + sizeof(reply)] = {};
+        len = strlen(reply);
+        memcpy(writebuf, &len, 4);
+        memcpy(&writebuf[4], reply, len);
+        return write_full(connfd, writebuf, 4 + len);
     };
 
     public:
@@ -50,7 +71,12 @@ class Server: Node {
                 log_error_and_abort("Error while accepting connection");
                 continue;
             }
-            handle_connection(connfd);
+            while (true) { // serve just one client for now
+                int err = single_request(connfd);
+                if (err) {
+                    break;
+                }
+            }
             close(connfd);
         }
     }
